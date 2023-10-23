f9be16436a56e617b99d1dacf13df7497e409a3e
Rasheeda Francis <rasheedafrancis02@gmail.com>
2023-10-22 22:21:15 -0500

    Added Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded',function(){
   const  Boardgame = document.getElementById("board")
   const  Boardsquares = Boardgame.getElementsByTagName('div');
+  const gamestate = new Array(9)
+  Move= Math.floor(Math.random()*(2-1+1))+1;
   for (const x of Boardsquares){
-    x.classList.add('square');
+    x.classList.add('square','empty');
+    x.addEventListener('click',function(){
+        if (Move==2){
+            x.textContent=('X');
+            x.classList.add('X');
+            gamestate[x] ='X';
+            Move =1;
+        }
+        else{
+            x.textContent=('O');
+            x.classList.add('O');
+            gamestate[x] ='O';
+            Move =2;
+        }
 
+
+    
+
+    });
 
   }
 
